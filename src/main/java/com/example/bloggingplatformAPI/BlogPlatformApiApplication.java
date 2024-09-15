c97856aee7d4aceddc1e070aafcb66417b863abf
Mateus Felipe <brasil.mateus748@gmail.com>
2024-09-14 23:48:43 -0300

    Fix file's directory

--- src/main/java/com/example/bloggingplatformAPI/BlogPlatformApiApplication.java
+++ src/main/java/com/example/bloggingplatformAPI/BlogPlatformApiApplication.java
@@ -1,4 +1,4 @@
-package com.blog_platform.api;
+package com.example.bloggingplatformAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
