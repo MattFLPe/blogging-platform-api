74b13e748cb9364fb803988eba56acbd89341ec8
Mateus Felipe <brasil.mateus748@gmail.com>
2024-09-26 09:55:45 -0300

    Add import and ComponentScan annotation

--- src/main/java/com/example/bloggingplatformAPI/BlogPlatformApiApplication.java
+++ src/main/java/com/example/bloggingplatformAPI/BlogPlatformApiApplication.java
@@ -2,10 +2,12 @@ package com.example.bloggingplatformAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan
 public class BlogPlatformApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlogPlatformApiApplication.class, args);
 	}
