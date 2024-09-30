dd10cf08258cd170f74767004bc05ce27435ff51
Mateus Felipe <brasil.mateus748@gmail.com>
2024-09-29 23:13:46 -0300

    Delete one import and one annotation

--- src/main/java/com/example/bloggingplatformAPI/BlogPlatformApiApplication.java
+++ src/main/java/com/example/bloggingplatformAPI/BlogPlatformApiApplication.java
@@ -2,11 +2,9 @@ package com.example.bloggingplatformAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan
 public class BlogPlatformApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogPlatformApiApplication.class, args);
