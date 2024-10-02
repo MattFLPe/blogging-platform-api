d3f72dede44af795c12dfc8dedb2eb59d68b99cd
Mateus Felipe <brasil.mateus748@gmail.com>
2024-10-02 04:11:34 -0300

    Define custom query to search for specific terms

--- src/main/java/com/example/bloggingplatformAPI/BlogPostRepository.java
+++ src/main/java/com/example/bloggingplatformAPI/BlogPostRepository.java
@@ -1,9 +1,16 @@
 package com.example.bloggingplatformAPI;
 import com.example.bloggingplatformAPI.model.BlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
-
+    @Query("SELECT b FROM BlogPost b WHERE " +
+            "LOWER(b.title) LIKE LOWER(CONCAT('%', :term, '%')) OR " +
+            "LOWER(b.content) LIKE LOWER(CONCAT('%', :term, '%')) OR " +
+            "LOWER(b.category) LIKE LOWER(CONCAT('%', :term, '%'))")
+    List<BlogPost> searchByTerm(String term);
 }
