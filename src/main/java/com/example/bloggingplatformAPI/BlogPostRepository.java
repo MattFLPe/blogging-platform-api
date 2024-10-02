07b29dcac2757b23bd1ba8757f7bf7ec39546a30
Mateus Felipe <brasil.mateus748@gmail.com>
2024-10-02 10:00:57 -0300

    Add @Param annotation

--- src/main/java/com/example/bloggingplatformAPI/BlogPostRepository.java
+++ src/main/java/com/example/bloggingplatformAPI/BlogPostRepository.java
@@ -2,6 +2,7 @@ package com.example.bloggingplatformAPI;
 import com.example.bloggingplatformAPI.model.BlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,5 @@ public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
             "LOWER(b.title) LIKE LOWER(CONCAT('%', :term, '%')) OR " +
             "LOWER(b.content) LIKE LOWER(CONCAT('%', :term, '%')) OR " +
             "LOWER(b.category) LIKE LOWER(CONCAT('%', :term, '%'))")
-    List<BlogPost> searchByTerm(String term);
+    List<BlogPost> searchByTerm(@Param("term") String term);
 }
